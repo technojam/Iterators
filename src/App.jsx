54b59d7b92d24e89e2c4e2d60e6da54905333da0
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-08-29 02:41:11 +0530

    🐛 fix footer bug
    updated footer css and logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
-
 function App() {
   return (
     <>
     <Navbar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/admin" component={Admin} />
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+      </Switch>
     <Footer />
     </>
   );
